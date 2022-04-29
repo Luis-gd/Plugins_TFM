415d4e941b379fc89139ae76d69676450d581de2
aledomcam <103251155+aledomcam@users.noreply.github.com>
2022-04-29 13:47:13 +0200

    Añadir métodos para calcular media y desviación típica de una lista de valores

--- src/main/java/earlywarn/main/Utils.java
+++ src/main/java/earlywarn/main/Utils.java
@@ -1,5 +1,7 @@
 package earlywarn.main;
 
+import java.util.List;
+
 /**
  * Clase que almacena utilidades varias
  */
@@ -20,5 +22,46 @@ public class Utils {
 		} else {
 			throw new IllegalArgumentException("El resultado especificado no es un Long ni un Double");
 		}
+	}
+
+	/**
+	 * Calcula la media de una lista de valores decimales
+	 * @param valores Lista de valores
+	 * @return Media de la lista de valores
+	 */
+	public static double getMedia(List<Double> valores) {
+		double total = 0;
+		for (Double valor : valores) {
+			total += valor;
+		}
+		return total / valores.size();
+	}
+
+	/**
+	 * Calcula la desviación típica de una lista de valores decimales
+	 * @param valores Lista de valores
+	 * @return Desvuación típica de la lista de valores
+	 */
+	public static double getStd(List<Double> valores) {
+		double totalCuadrados = 0;
+		double media = getMedia(valores);
+		for (Double valor : valores) {
+			totalCuadrados += Math.pow(valor - media, 2);
+		}
+		return Math.sqrt(totalCuadrados / valores.size());
+	}
+
+	/**
+	 * Calcula la desviación típica máxima posible en un conjunto de datos del tamaño indicado, asumiendo que todos los
+	 * elementos toman valores entre 0 y 1.
+	 * @param númeroDatos Número de elementos en el conjunto de datos
+	 * @return Desviación típica máxima que puede obtenerse variando los valores del conjunto de datos entre 0 y 1
+	 */
+	public static double getStdMáxima(int númeroDatos) {
+		int numElemAltos = (int) Math.floor(númeroDatos);
+		int numElemBajos = (int) Math.ceil(númeroDatos);
+		float media = (float) numElemAltos / númeroDatos;
+
+		return Math.sqrt(((1 - media) * numElemAltos + (0 - media) * numElemBajos) / númeroDatos);
 	}
 }
