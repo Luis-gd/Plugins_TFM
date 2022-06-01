19abbb495ac8750001959e5cd7f3be154fcda0b2
davidse9 <davidse8@gmail.com>
2022-06-01 20:37:06 +0200

    Terminada la implementación de las restricciones

--- src/main/java/earlywarn/mh/Criterios.java
+++ src/main/java/earlywarn/mh/Criterios.java
@@ -1,5 +1,11 @@
 package earlywarn.mh;
 
+import javax.validation.constraints.Null;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
 public class Criterios{
     //Número de conexiones con las que trabajamos
     private static int numDimensions=0;
@@ -8,26 +14,32 @@ public class Criterios{
     //TODO: Añadir la opción de meter las restricciones por parámetros
     //Restricción del impacto económico de los pasajeros perdidos, en porcentaje
     private static float maxPorcentajePasajerosPerdidos=0.2f;
+    //Restricción de la homogeneidad en el porcentaje de pasajeros que pierden las aerolíneas
+    private static float maxPorcentajeDesviacionMediaPasajerosPerdidosPorCompanyia=0.2f;
+    //Restricción de la homogeneidad en el porcentaje de pérdida de ingresos por turismo en los destinos.
+    private static float maxPorcentajeDesviacionMediaIngresosDestinos=0.2f;
+    //Restricción sobre la conectividad perdida en los destinos.
+    private static float maxPorcentajeConectividadPerdida=0.2f;
     //Restricción del porcentaje de pérdida de ingresos por turismo en los destinos
     private static float maxPorcentajeDineroPerdidoRegion = 0.2f;
     //TODO: Calcular un valor correcto para la penalización
     //Valor que se añade a la función objetivo cuando una solución no cumple una restricción
-    private static int penalizacionRestriccion=1000;
+    private final static int penalizacionRestriccion=1000;
     //La solución con la que trabajamos, se modifica en evaluate fitness
     private static boolean[] solucion;
     //TODO: Hacer la función evaluteFitness
-    //TODO: Hay que cargar todos los datos, ahora mismo los estoy dando por hecho
+    //TODO: Hay que cargar todos los datos, ahora mismo los estoy dando por hecho, cada dimensión una conexión
     /**
      * Calcula el fitness del riesgo importado dada una solución
      * @return Devuelve el riesgo de las conexiones abiertas
      */
     //TODO: Comprobar las escalas de riesgo
-    //TODO: Comprobar si debería estar en porcentaje para normalizarlos distintos objetivos
+    //TODO: Comprobar si debería estar en porcentaje para normalizar los distintos objetivos
     private static float calculoRiesgoImportado(){
         float[] riesgoConexion=new float[numDimensions];
         float riesgo=0;
         for(int i=0;i<numDimensions;i++){
-            if(solucion[i]==true){
+            if(solucion[i]){
                 riesgo=riesgo+riesgoConexion[i];
             }
         }
@@ -76,21 +88,142 @@ public class Criterios{
         }
         return 0;
     }
-    //TODO: Arreglar la condición, liandola mirando todas las compañías
+
+    /**
+     * Se comprueba la homogeneidad de perdida de pasajeros entre distintas compañías aéreas, para ello hacemos la desviación
+     * media de los pasajeros perdidos por cada compañía y vemos si esta debajo de un porcentaje, implementado como restricción
+     * @return Devuelve el valor de penalizaciónRestriccion si no se cumple la restricción, sino 0
+     */
     private static int calculoHomogeneidadPasajerosAerolineas(){
+        int i;
         int[][] pasajerosConexion=new int[numDimensions][numCompanyias];
         int totalPasajeros[]=new int[numCompanyias];
         int totalPasajerosConexiones[]=new int[numCompanyias];
+        float porcentajePerdido[] = new float[numCompanyias];
+        float porcentajePerdidoMedia = 0.0f;
+        float porcentajePerdidoDesviacionMedia = 0.0f;
         for(int j=0;j<numCompanyias;j++){
-            for(int i=0;i<numDimensions;i++){
+            for(i=0;i<numDimensions;i++){
                 totalPasajeros[j]=totalPasajeros[j]+pasajerosConexion[i][j];
-                if(solucion[i]==true){
+                if(solucion[i]){
                     totalPasajerosConexiones[j]=totalPasajerosConexiones[j]+pasajerosConexion[i][j];
                 }
             }
+            porcentajePerdido[j]=1-(float)totalPasajerosConexiones[j]/totalPasajeros[j];
+            porcentajePerdidoMedia = porcentajePerdidoMedia + porcentajePerdido[j];
         }
-        float porcentajePerdido=1-totalPasajerosConexiones/totalPasajeros;
-        if(porcentajePerdido>maxPorcentajePasajerosPerdidos){
+        porcentajePerdidoMedia = porcentajePerdidoMedia / numCompanyias;
+        for(i=0;i<numCompanyias;i++){
+            porcentajePerdidoDesviacionMedia = porcentajePerdidoDesviacionMedia +
+                                                Math.abs(porcentajePerdido[i]-porcentajePerdidoMedia);
+        }
+        porcentajePerdidoDesviacionMedia = porcentajePerdidoDesviacionMedia / numCompanyias;
+        if(porcentajePerdidoDesviacionMedia>maxPorcentajeDesviacionMediaPasajerosPerdidosPorCompanyia) {
+            return penalizacionRestriccion;
+        }
+        return 0;
+    }
+
+    /**
+     * Se comprueba la homogeneidad de perdida de ingresos por turismo entre los diferentes destinos, para ello hacemos la
+     * desviación media de los pasajeros perdidos por cada compañía y vemos si esta debajo de un porcentaje, implementado
+     * como restricción
+     * @return Devuelve el valor de penalizaciónRestriccion si no se cumple la restricción, sino 0
+     */
+    private static int calculoHomogeneidadIngresosTurismoDestinos(){
+        int[] numeroVuelos = new int[numDimensions];
+        String[] aeropuertosEntradaRepetidos = new String[numDimensions];
+        java.util.Map<String, Integer> numVuelosAeropuerto = new java.util.HashMap<>();
+        java.util.Map<String, Integer> numVuelosAeropuertoConexiones = new java.util.HashMap<>();
+        List<Float> porcentajePerdido = new ArrayList<>();
+        float mediaPorcentajeVuelosPerdidos=0.0f;
+        float porcentajePerdidoDesviacionMedia = 0.0f;
+        int i;
+        for(i=0;i<numDimensions;i++){
+            if(numVuelosAeropuerto.get(aeropuertosEntradaRepetidos[i])!=null){
+                numVuelosAeropuerto.put(aeropuertosEntradaRepetidos[i], numVuelosAeropuerto.get(aeropuertosEntradaRepetidos[i])+
+                        numeroVuelos[i]);
+            }else{
+                numVuelosAeropuerto.put(aeropuertosEntradaRepetidos[i], numeroVuelos[i]);
+            }
+            if(solucion[i]){
+                if(numVuelosAeropuertoConexiones.get(aeropuertosEntradaRepetidos[i])!=null){
+                    numVuelosAeropuertoConexiones.put(aeropuertosEntradaRepetidos[i],
+                            numVuelosAeropuertoConexiones.get(aeropuertosEntradaRepetidos[i])+numeroVuelos[i]);
+                }else{
+                    numVuelosAeropuertoConexiones.put(aeropuertosEntradaRepetidos[i], numeroVuelos[i]);
+                }
+            }
+        }
+        i=0;
+        for (String key:numVuelosAeropuerto.keySet()) {
+            if(numVuelosAeropuertoConexiones.get(key)!=null){
+                porcentajePerdido.add((float)numVuelosAeropuertoConexiones.get(key)/numVuelosAeropuerto.get(key));
+                mediaPorcentajeVuelosPerdidos=mediaPorcentajeVuelosPerdidos+porcentajePerdido.get(i);
+            }else{
+                porcentajePerdido.add(0.0f);
+            }
+            i++;
+        }
+        mediaPorcentajeVuelosPerdidos = mediaPorcentajeVuelosPerdidos / porcentajePerdido.size();
+        for(i=0;i<porcentajePerdido.size();i++){
+            porcentajePerdidoDesviacionMedia=porcentajePerdidoDesviacionMedia+
+                                                Math.abs(porcentajePerdido.get(i)-mediaPorcentajeVuelosPerdidos);
+        }
+        porcentajePerdidoDesviacionMedia = porcentajePerdidoDesviacionMedia / porcentajePerdido.size();
+        if(porcentajePerdidoDesviacionMedia>maxPorcentajeDesviacionMediaIngresosDestinos) {
+            return penalizacionRestriccion;
+        }
+        return 0;
+    }
+    //TODO: Pasar a función auxiliar el código compartido entre calculoConectividadDestinos y calculoHomogeneidadIngresosTurismoDestinos
+    /**
+     * Comprueba que la conectividad perdida en los destinos no sea mayor que un porcentaje, implementada como una restricción
+     * @return Devuelve el valor de penalizaciónRestriccion si no se cumple la restricción, sino 0
+     */
+    private static int calculoConectividadDestinos(){
+        int[] numeroVuelos = new int[numDimensions];
+        String[] aeropuertosEntradaRepetidos = new String[numDimensions];
+        java.util.Map<String, Integer> conectividadAeropuerto = new java.util.HashMap<>();
+        java.util.Map<String, Integer> numVuelosAeropuerto = new java.util.HashMap<>();
+        java.util.Map<String, Integer> numVuelosAeropuertoConexiones = new java.util.HashMap<>();
+        List<Float> porcentajePerdido = new ArrayList<>();
+        int conectividadTotal=0;
+        float sumaConectividadConexiones=0.0f;
+        float porcentajeConectividadPerdida=0;
+        int i;
+        for(i=0;i<numDimensions;i++){
+            if(numVuelosAeropuerto.get(aeropuertosEntradaRepetidos[i])!=null){
+                numVuelosAeropuerto.put(aeropuertosEntradaRepetidos[i], numVuelosAeropuerto.get(aeropuertosEntradaRepetidos[i])+
+                        numeroVuelos[i]);
+            }else{
+                numVuelosAeropuerto.put(aeropuertosEntradaRepetidos[i], numeroVuelos[i]);
+            }
+            if(solucion[i]){
+                if(numVuelosAeropuertoConexiones.get(aeropuertosEntradaRepetidos[i])!=null){
+                    numVuelosAeropuertoConexiones.put(aeropuertosEntradaRepetidos[i],
+                            numVuelosAeropuertoConexiones.get(aeropuertosEntradaRepetidos[i])+numeroVuelos[i]);
+                }else{
+                    numVuelosAeropuertoConexiones.put(aeropuertosEntradaRepetidos[i], numeroVuelos[i]);
+                }
+            }
+        }
+        i=0;
+        for (String key:numVuelosAeropuerto.keySet()) {
+            if(numVuelosAeropuertoConexiones.get(key)!=null){
+                porcentajePerdido.add(conectividadAeropuerto.get(i)*
+                        (float)numVuelosAeropuertoConexiones.get(key)/numVuelosAeropuerto.get(key));
+            }else{
+                porcentajePerdido.add(0.0f);
+            }
+            i++;
+        }
+        for(i=0;i<porcentajePerdido.size();i++){
+            sumaConectividadConexiones=sumaConectividadConexiones+porcentajePerdido.get(i);
+            conectividadTotal=conectividadTotal+conectividadAeropuerto.get(i);
+        }
+        porcentajeConectividadPerdida=1-sumaConectividadConexiones/conectividadTotal;
+        if(porcentajeConectividadPerdida>maxPorcentajeConectividadPerdida) {
             return penalizacionRestriccion;
         }
         return 0;
